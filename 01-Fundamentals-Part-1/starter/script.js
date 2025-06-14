43b6708d1a395ab7b39f5be631ea48e0426268d7
MaxOkhten <maxokhten@gmail.com>
2025-06-14 16:13:21 +0200

    finish 1st section

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,9 @@
+// let js = 'amazing';
+
+//       console.log(js);
+
+const age = 13;
+age >= 18 ? console.log("Hello!") : console.log("Not Hello!");
+
+const hello = age >= 18 ? "Hello!" : "Not Hello!";
+console.log(hello); 
